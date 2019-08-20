6bd8d9865af2037ea6e923d0a61d849bb0a3db60
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-20 13:52:03 -0600

    Imported axios and react-router-dom dependencies. Created the Login component

---
+++ friends/src/components/Login.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChange = e => {
+        this.setState({})
+    };
+
+    login = e => {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Log in</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
